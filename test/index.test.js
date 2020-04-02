c270558dd5eb99286d48040e593f2c8a9caafd16
Sajjad Shiray <shirazy.sajjad@gmail.com>
2020-04-02 10:33:46 +0430

    test

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-import CSV from './index';
+import CSV from '../src/index';
 
 const data = `Username; Identifier;First name;Last name
 booker12;9012;Rachel;Booker
@@ -9,7 +9,6 @@ smith79;5079;Jamie;Smith
 `;
 test('CSV to Array', function() {
     const csv = new CSV(data, ';');
-    const array = csv.toArray();
     expect(csv.toArray()).toEqual([
         ['Username', ' Identifier', 'First name', 'Last name'],
         ['booker12', '9012', 'Rachel', 'Booker'],
